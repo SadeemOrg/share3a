cc13ccd4d281b6383c1a8d23cb9090c3b1a5b58d
zeyadaverotech <98884466+zeyadaverotech@users.noreply.github.com>
2023-07-13 20:25:48 +0300

    add form

--- app/Nova/Form.php
+++ app/Nova/Form.php
@@ -2,37 +2,33 @@
 
 namespace App\Nova;
 
-use App\Models\Form;
 use Illuminate\Http\Request;
-use Illuminate\Validation\Rules;
-use Laravel\Nova\Fields\Gravatar;
+use Laravel\Nova\Fields\HasMany;
 use Laravel\Nova\Fields\ID;
-use Laravel\Nova\Fields\Password;
 use Laravel\Nova\Fields\Text;
 use Laravel\Nova\Http\Requests\NovaRequest;
-use Outl1ne\MultiselectField\Multiselect;
+use Whitecube\NovaFlexibleContent\Flexible;
 
-class User extends Resource
+class Form extends Resource
 {
 
     public static function label()
     {
-        return __('ادارة المستخدمين ');
+        return __('ادارة صفحات الهبوط ');
     }
-
     /**
      * The model the resource corresponds to.
      *
-     * @var class-string<\App\Models\User>
+     * @var class-string<\App\Models\Form>
      */
-    public static $model = \App\Models\User::class;
+    public static $model = \App\Models\Form::class;
 
     /**
      * The single value that should be used to represent the resource when being displayed.
      *
      * @var string
      */
-    public static $title = 'name';
+    public static $title = 'id';
 
     /**
      * The columns that should be searched.
@@ -40,9 +36,8 @@ class User extends Resource
      * @var array
      */
     public static $search = [
-        'id', 'name', 'email',
+        'id',
     ];
-
 
     /**
      * Get the fields displayed by the resource.
@@ -50,42 +45,50 @@ class User extends Resource
      * @param  \Laravel\Nova\Http\Requests\NovaRequest  $request
      * @return array
      */
+
+     public static function indexQuery(NovaRequest $request, $query)
+     {
+
+        //  dd(in_array("1",  $request->user()->userrole()));/
+        //  dd(in_array("1",json_decode( $request->user()->userrole())));
+// dd(gettype( $request->user()->userrole()));
+
+         return $query->wherein('id',   json_decode( $request->user()->userrole()));
+
+     }
     public function fields(NovaRequest $request)
     {
         return [
             ID::make()->sortable(),
+            Text::make('Name', 'name'),
+            Text::make('slug', 'slug'),
+            Text::make('text', 'text'),
+            Text::make('sup_text', 'sup_text'),
+            Flexible::make('questions', 'questions')
+                ->addLayout('Add select', 'select', [
+                    Text::make('name'),
+                    Flexible::make('select', 'selectform')
+                        ->addLayout('Add select choices', 'choices', [
+                            Text::make('text'),
 
-            Gravatar::make()->maxWidth(50),
+                        ])
 
-            Text::make('Name')
-                ->sortable()
-                ->rules('required', 'max:255'),
-
-            Text::make('Email')
-                ->sortable()
-                ->rules('required', 'email', 'max:254')
-                ->creationRules('unique:users,email')
-                ->updateRules('unique:users,email,{{resourceId}}'),
+                ])->addLayout('Add text', 'text', [
+                    Text::make('text'),
 
 
-            // Async model querying
-            Multiselect::make('Form Admin', 'roles')
-            ->options(function(){
-              $forms=  Form::all();
-              $address_type_admin_array =  array();
+                ])->addLayout('Add Note Filed', 'Note', [
+                    Text::make('text'),
 
-              foreach ($forms as $forms) {
+                ])->addLayout('Add boolean Filed', 'boolean', [
+                    Text::make('text'),
 
-                  $address_type_admin_array += [$forms['id'] => ($forms['name'])];
+                ]),
 
-              }
-                return $address_type_admin_array;
-            })
-                ,
-            Password::make('Password')
-                ->onlyOnForms()
-                ->creationRules('required', Rules\Password::defaults())
-                ->updateRules('nullable', Rules\Password::defaults()),
+
+            HasMany::make(__("FormResults"), "FormResults", \App\Nova\FormResults::class)
+
+
         ];
     }
 
