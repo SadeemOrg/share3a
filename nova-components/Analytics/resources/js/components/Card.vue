60861836b5c6e63ae296665abf577d80e5767350
Nserallah <moslem.12166@gmail.com>
2023-07-27 15:21:55 +0300

    add searchable dropdown

--- nova-components/Analytics/resources/js/components/Card.vue
+++ nova-components/Analytics/resources/js/components/Card.vue
@@ -1,12 +1,17 @@
 <template>
     <Card class="flex  flex-row gap-x-6 items-center justify-center">
-
-
+        <model-select :options="options"
+                                v-model="item"
+                                placeholder="select item">
+         </model-select>
+         {{ item.text }}
 
     </Card>
 </template>
 
 <script>
+import { ModelSelect } from 'vue-search-select'
+import 'vue-search-select/dist/VueSearchSelect.css'
 export default {
     props: [
         'card',
@@ -16,6 +21,33 @@ export default {
         // 'resourceId',
         // 'resourceName',
     ],
+    components: {ModelSelect},
+    data () {
+      return {
+        options: [
+          { value: '1', text: 'aa' + ' - ' + '1' },
+          { value: '2', text: 'ab' + ' - ' + '2' },
+          { value: '3', text: 'bc' + ' - ' + '3' },
+          { value: '4', text: 'cd' + ' - ' + '4' },
+          { value: '5', text: 'de' + ' - ' + '5' }
+        ],
+        item: {
+          value: '',
+          text: ''
+        },
+
+      }
+    },
+    methods: {
+      reset () {
+        this.item = {}
+      },
+      selectFromParentComponent1 () {
+        // select option from parent component
+        this.item = this.options[0]
+      },
+
+    },
 
     mounted() {
         //
