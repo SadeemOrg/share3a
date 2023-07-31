cd01b32d583cf86b55e978aa31af42c905574e31
zeyadaverotech <98884466+zeyadaverotech@users.noreply.github.com>
2023-07-31 13:14:13 +0300

    add

--- resources/views/home1.blade.php
+++ resources/views/home1.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>كلية الدعوة</title>
+    <title>{{ $forms->slug }}</title>
     <!-- Styles -->
     <link rel="stylesheet" type="text/css" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css">
     @vite('resources/css/app.css')
