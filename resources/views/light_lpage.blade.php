09f8df28e2f7007a884be47592b53937fd2da652
Nserallah <dev.n.mot@gmail.com>
2023-08-10 13:35:44 +0300

    pull main branch

--- resources/views/light_lpage.blade.php
+++ resources/views/light_lpage.blade.php
@@ -474,7 +474,22 @@
 
         $( document ).ready(function() {
             // validation form 
-            $("#register_form").validate();
+            $("#register_form").validate({
+                rules: {
+                    name: "required",
+                    email: {
+                    required: true,
+                    email: true
+                    }
+                },
+                messages: {
+                    name: "Please specify your name",
+                    email: {
+                    required: "We need your email address to contact you",
+                    email: "Your email address must be in the format of name@domain.com"
+                    }
+                }
+            });
             // =============================================== //
             $('.modal-content').click(function(event) {
                 event.stopPropagation()
