097b73631c0bff66df85e845dea58f3392f6ecb8
ameedasmah1 <ameed.a@averotech.com>
2023-12-25 12:14:50 +0200

    trynow

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   ],
   resolve: {
     alias: {
-      'vue': 'vue/dist/vue.esm-bundler.js',
+      'vue': 'vue/dist/vue.esm.js', // Change to non-minified version
     },
   },
 });
