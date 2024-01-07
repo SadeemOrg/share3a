4c06e08757f1321d378f6e44bfc51bcde87a111b
ameedasmah1 <ameed.a@averotech.com>
2024-01-07 13:50:51 +0200

    Modify some changes

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -48,16 +48,16 @@
                         </div>
                     </div>
                 @elseif($nqp == false && $sakhnin == true && $share3a == false)
-                    <div class="grid grid-cols-3 gap-6 mt-6">
-                        <div class="col-span-1 flex justify-center items-center">
+                    <div class="grid grid-cols-2 sm:grid-cols-3 gap-6 mt-6 mb-8 sm:mb-0">
+                        <div class="col-span-2 sm:col-span-1 flex justify-center items-center">
                             <img class="max-w-[220px] w-full mx-auto"
                                 src="{{ asset('assets/images/header-logos/share3a-sakhnin_2.jpeg') }}" alt="left logo">
                         </div>
-                        <div class="col-span-1 flex justify-center items-center">
+                        <div class="col-span-2 sm:col-span-1 flex justify-center items-center">
                             <img class="max-w-[220px] w-full mx-auto"
                                 src="{{ asset('assets/images/header-logos/share3a-sakhnin_1.jpeg') }}" alt="left logo">
                         </div>
-                        <div class="col-span-1 flex justify-center items-center">
+                        <div class="col-span-2 sm:col-span-1 flex justify-center items-center">
                             <img class="max-w-[220px] w-full mx-auto"
                                 src="{{ asset('assets/images/header-logos/middlelogo-nqp1.jpeg') }}" alt="left logo">
                         </div>
@@ -370,7 +370,7 @@
                                     0506300535</a>
                             </div>
                         @elseif($nqp == false && $share3a == false && $sakhnin == true)
-                            <div class="flex flex-row gap-x-4 justify-start">
+                            <div class="flex flex-row flex-wrap sm:flex-nowrap gap-y-2 sm:gap-y-0 gap-x-4 justify-start">
                                 <a href="tel:0546896365" class="underline underline-offset-1">الشيخ فريد حمزة:
                                     0546896365</a>
                                 <a href="tel:0506300535" class="underline underline-offset-1">نجاح عثمان:
