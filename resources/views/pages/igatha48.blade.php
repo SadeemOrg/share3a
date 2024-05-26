71c1886c2322cd3bf4d4695011f2236ffe66897c
zeyadaverotech <zeyad.hawash1@hotmail.com>
2024-05-26 15:29:39 +0300

    Modify changes in igatha form

--- resources/views/pages/igatha48.blade.php
+++ resources/views/pages/igatha48.blade.php
@@ -2,7 +2,7 @@
 @section('content')
     <div class="min-h-screen igatha48"
         style="background-image: url({{ asset('assets/images/website-bg-img.webp') }}); background-size: auto 100%;">
-        <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+        <div class="mx-auto max-w-7xl px-2 sm:px-6 lg:px-8">
             <div class="flex flex-row items-center justify-center ">
                 <div class="col-span-1 flex justify-center items-center">
                     <img class="max-w-sm max-h-96 mx-auto my-14" src="{{ asset('assets/images/header-logos/igatheh48.png') }}"
@@ -10,7 +10,7 @@
                 </div>
             </div>
             <section dir="rtl">
-                <div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                <div class="mx-auto max-w-7xl px-2   sm:px-6 lg:px-8">
                     <div class="mx-auto max-w-4xl">
                         <div class="text-white text-center">
                             <p class="text-[25px] md:text-[40px] lg:text-[59px] bg-[#EBBD22] py-4 px-4 text-sh -mb-1">
@@ -26,7 +26,7 @@
                             $questions = $forms->questions;
                             $questions = json_decode($questions);
                         @endphp
-                        <ul class="text-white features-list mt-10 pb-8 mb-8  ">
+                        <ul class="text-white features-list mt-10 mb-8  ">
                             <li class="mb-5">
                                 <p class="text-[20px] md:text-[24px] lg:text-[27px] relative pr-10">
                                     سجّلوا تفاصيلكم هنا وسنعمل كل جهدنا لنوصل إليكم لحوم الأضاحي من المتبرّعين خلال أولى
@@ -34,24 +34,17 @@
                                 </p>
                             </li>
                         </ul>
-                        <div class="pt-10 md:pt-20 lg:pt-28 flex flex-col items-center justify-start relative ">
+                        <div class="pt-10  flex flex-col items-center justify-start relative ">
                             <div
                                 class="block1 block absolute left-[50%] translate-x-[-50%] top-[20px] md:top-[40px] lg:top-[45px] w-[85%] sm:w-[75%] md:w-[70%] lg:w-[795px]">
-                                <div class="relative ">
-                                    <h3
-                                        class=" text-white  text-[24px] sm:text-[40px] md:text-[50px] lg:text-[62px] text-center z-10 relative italic">
-                                        جمعية الأغاثة 48</h1>
-                                        <img src="{{ asset('assets/images/smallBlackArea.svg') }}" alt="black_svg"
-                                            class="imgform ">
-                                </div>
                             </div>
                             <form method="post" action="{{ route('form.store') }}"
                                 class="pb-28  flex flex-col items-center  w-full">
                                 @csrf
-                                <div class=" bg-[#EBBD22] w-[95%] h-auto flex flex-col px-4 md:px-14 pb-8 mx-auto ">
-                                   <div class="w-full mt-10 sm:mt-14 lg:mt-20 xl:mt-28">
-                                  
-                                </div>
+                                <div
+                                    class=" bg-[#EBBD22] w-[95%] h-auto flex flex-col px-4 md:px-14 pb-8 mx-auto rounded-md">
+                                    <div class="w-full mt-10 sm:mt-12 lg:mt-20 xl:mt-12">
+                                    </div>
                                     <input type="hidden" name='formid' value="{{ $forms->id }}">
                                     @csrf
                                     @foreach ($questions as $question)
@@ -60,7 +53,7 @@
                                                 <div class="w-full px-3">
                                                     <div class="relative">
                                                         <select name='{{ $question->attributes->name }}'
-                                                            class="bg-transparent w-full border border-gray-200 text-white py-4 px-4 pr-8 rounded-sm leading-tight focus:outline-none focus:bg-transparent focus:ring-0 focus:outline-transparent focus:border-[#EBBD22]"
+                                                            class="bg-transparent w-full border border-gray-200 text-[#124a85] py-4 px-4 pr-8 rounded-sm leading-tight focus:outline-none focus:bg-transparent focus:ring-0 focus:outline-transparent focus:border-[#EBBD22]"
                                                             id="grid-state">
                                                             <option class="text-[#000]">
                                                                 {{ $question->attributes->name }}</option>
@@ -77,9 +70,9 @@
                                         @endif
                                         @if ($question->layout == 'text')
                                             <div class="flex flex-wrap -mx-3 mb-6 mt-3">
-                                                <div class="w-full px-3">
+                                                <div class="w-full md:px-3">
                                                     <input required name="{{ $question->attributes->text }}"
-                                                        class="w-full md:h-[65px] bg-transparent border-transparent text-white font-medium border border-white rounded-md focus:ring-white focus:border-white sm:text-sm p-4"
+                                                        class="w-full h-[35px] md:h-[65px] border-transparent text-[#124a85] font-semibold border border-text-[#124a85] rounded-md focus:ring-text-[#124a85] focus:border-text-[#124a85] sm:text-base p-4"
                                                         id="grid-password" type="text"
                                                         placeholder="{{ $question->attributes->text }}"
                                                         style="color: rgb(99, 89, 89); /* Change placeholder color to black */">
@@ -128,7 +121,7 @@
                                         @endif
                                     @endforeach
                                     <button type="submit"
-                                        class="mt-5 bg-[#124A85] text-white text-base sm:text-xl w-[100%]  md:h-[75px] py-4 font-[700] hover:bg-[#101426] duration-200">اضغط
+                                        class="mt-5 bg-[#124A85] text-white text-base sm:text-xl w-[100%]  md:h-[75px] py-3 sm:py-4 rounded-md font-[700] hover:bg-[#101426] duration-200">اضغط
                                         لإكمال عملية التسجيل</button>
                                 </div>
                             </form>
