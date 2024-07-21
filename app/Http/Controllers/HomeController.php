283b457b9a873f64a1b1f59a6a1c8b38f45294c2
zeyadaverotech <zeyad.hawash1@hotmail.com>
2024-07-21 14:37:34 +0300

    add formStoreTaweieh

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -126,7 +126,137 @@ class HomeController extends Controller
             return view('pages.igatha48-success-form', compact('page_title'));
         }
 
-        if($id==23) {
+        if ($id == 23) {
+            return response()->json([
+                'status' => 'success',
+                'message' => 'Page loaded successfully',
+            ], 200);
+        }
+
+        if ($form->type == 1)
+            return view('thanks');
+        return view('light_thanks', compact('form'));
+    }
+    public function formStoreTaweieh(Request $request)
+    {
+        $this->validate($request, [
+            'الاسم الكامل' => 'required',
+            'الهاتف' => ['required', 'regex:/^\+?\d{10,14}$/'],
+            'note' => 'required',
+        ], [
+            'الاسم الكامل.required' => 'الرجاء ادخال اسم',
+            'الهاتف.required' => 'ارجاء ادخال رقم الهاتف',
+            'الهاتف.regex' => 'الرجاء ادخال رقم الهاتف بشكل صحيح. ',
+            'note.required' => 'الرجاء ادخال البلد',
+        ]);
+
+        $ip = $_SERVER['REMOTE_ADDR'];
+        //Deep detect ip
+        if (filter_var(@$_SERVER['HTTP_FORWARDED'], FILTER_VALIDATE_IP)) {
+            $ip = $_SERVER['HTTP_FORWARDED'];
+        }
+        if (filter_var(@$_SERVER['HTTP_FORWARDED_FOR'], FILTER_VALIDATE_IP)) {
+            $ip = $_SERVER['HTTP_FORWARDED_FOR'];
+        }
+        if (filter_var(@$_SERVER['HTTP_X_FORWARDED_FOR'], FILTER_VALIDATE_IP)) {
+            $ip = $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+        if (filter_var(@$_SERVER['HTTP_CLIENT_IP'], FILTER_VALIDATE_IP)) {
+            $ip = $_SERVER['HTTP_CLIENT_IP'];
+        }
+        if (filter_var(@$_SERVER['HTTP_X_REAL_IP'], FILTER_VALIDATE_IP)) {
+            $ip = $_SERVER['HTTP_X_REAL_IP'];
+        }
+        if (filter_var(@$_SERVER['HTTP_CF_CONNECTING_IP'], FILTER_VALIDATE_IP)) {
+            $ip = $_SERVER['HTTP_CF_CONNECTING_IP'];
+        }
+        if ($ip == '::1') {
+            $ip = '127.0.0.1';
+        }
+
+        $userAgent = $_SERVER['HTTP_USER_AGENT'];
+        $osPlatform = "Unknown OS Platform";
+        $osArray = array(
+            '/windows nt 10/i' => 'Windows 10',
+            '/windows nt 6.3/i' => 'Windows 8.1',
+            '/windows nt 6.2/i' => 'Windows 8',
+            '/windows nt 6.1/i' => 'Windows 7',
+            '/windows nt 6.0/i' => 'Windows Vista',
+            '/windows nt 5.2/i' => 'Windows Server 2003/XP x64',
+            '/windows nt 5.1/i' => 'Windows XP',
+            '/windows xp/i' => 'Windows XP',
+            '/windows nt 5.0/i' => 'Windows 2000',
+            '/windows me/i' => 'Windows ME',
+            '/win98/i' => 'Windows 98',
+            '/win95/i' => 'Windows 95',
+            '/win16/i' => 'Windows 3.11',
+            '/macintosh|mac os x/i' => 'Mac OS X',
+            '/mac_powerpc/i' => 'Mac OS 9',
+            '/linux/i' => 'Linux',
+            '/ubuntu/i' => 'Ubuntu',
+            '/iphone/i' => 'iPhone',
+            '/ipod/i' => 'iPod',
+            '/ipad/i' => 'iPad',
+            '/android/i' => 'Android',
+            '/blackberry/i' => 'BlackBerry',
+            '/webos/i' => 'Mobile'
+        );
+        foreach ($osArray as $regex => $value) {
+            if (preg_match($regex, $userAgent)) {
+                $osPlatform = $value;
+            }
+        }
+        $browser = "Unknown Browser";
+        $browserArray = array(
+            '/msie/i' => 'Internet Explorer',
+            '/firefox/i' => 'Firefox',
+            '/safari/i' => 'Safari',
+            '/chrome/i' => 'Chrome',
+            '/edge/i' => 'Edge',
+            '/opera/i' => 'Opera',
+            '/netscape/i' => 'Netscape',
+            '/maxthon/i' => 'Maxthon',
+            '/konqueror/i' => 'Konqueror',
+            '/mobile/i' => 'Handheld Browser'
+        );
+        foreach ($browserArray as $regex => $value) {
+            if (preg_match($regex, $userAgent)) {
+                $browser = $value;
+            }
+        }
+
+
+        $finallresult = array();
+        $data = $request->all();
+        // dd($data );
+        foreach ($data as $key => $value) {
+            if (!($key == 'formid' || $key == '_token')) {
+                $pus = array(
+                    'questionskey' => $key,
+                    'questionsanswerkey' => $value,
+                );
+                array_push($finallresult, $pus);
+            }
+        }
+        $FormResults = new FormResults();
+        $FormResults->form_id = $request->formid;
+        $FormResults->result = json_encode($finallresult);
+        $FormResults->user_ip = $ip;
+        $FormResults->browser = $browser;
+        $FormResults->os = $osPlatform;
+
+        $FormResults->save();
+        $form = Form::find($request->formid);
+        $id = $form->id;
+        $logo = $form->icons;
+        if ($id == 20)
+            return view('shabab-mwahdeh-success-form', compact('logo'));
+        if ($id == 21) {
+            $page_title = "جمعية الأغاثة 48";
+            return view('pages.igatha48-success-form', compact('page_title'));
+        }
+
+        if ($id == 23) {
             return response()->json([
                 'status' => 'success',
                 'message' => 'Page loaded successfully',
@@ -473,12 +603,11 @@ class HomeController extends Controller
 
         foreach ($data as $key => $value) {
             array_push($array, $value);
-
         }
         return $FormResults->form_id == 20
-        ? Excel::download(new ExportAlmowahde($array), 'users123.xlsx')
-        : Excel::download(new ExportFormReselt($array), 'users123.xlsx');
-            return;
+            ? Excel::download(new ExportAlmowahde($array), 'users123.xlsx')
+            : Excel::download(new ExportFormReselt($array), 'users123.xlsx');
+        return;
     }
     public function store(Request $request)
     {
