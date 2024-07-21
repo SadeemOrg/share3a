3ee30fc8e168abe503aa3e715d6f91d2678fb12b
ameedasmah1 <ameed.asmah1@gmail.com>
2024-07-21 14:25:42 +0300

    Modify some changes

--- resources/views/pages/Taweieh/index.blade.php
+++ resources/views/pages/Taweieh/index.blade.php
@@ -30,7 +30,8 @@
                     </div>
                     <div class=" flex flex-col items-center justify-center gap-y-3 z-10">
                         <p class="text-2xl font-black"> لتفاصيل أخرى عن الوظيفة</p>
-                        <button class="text-[#207ec6] text-3xl font-black cursor-pointer">أضغط هنا</لا>
+                        <a target="_black" href="{{ asset('assets/file/requierment-pdf.pdf') }}"
+                            class="text-[#207ec6] text-3xl font-black cursor-pointer">أضغط هنا</a>
                     </div>
                 </div>
             </div>
@@ -57,32 +58,33 @@
                 </div>
                 <div class=" flex flex-col items-center justify-center gap-y-3 z-10 mt-12">
                     <p class="text-2xl font-black"> لتفاصيل أخرى عن الوظيفة</p>
-                    <button class="text-[#207ec6] text-3xl font-black cursor-pointer">أضغط هنا</لا>
+                    <a target="_black" href="{{ asset('assets/file/requierment-pdf.pdf') }}"
+                        class="text-[#207ec6] text-3xl font-black cursor-pointer">أضغط هنا</a>
                 </div>
             </div>
             <div class="w-full mt-16 xl:mt-36 flex gap-x-2">
                 <form id="submission-form"
-                    class="flex flex-col items-center  max-w-3xl lg:mx-auto gap-y-2 md:gap-y-4 lg:basis-5/6 mx-6 w-full ">
+                    class="flex flex-col items-center max-w-3xl lg:mx-auto gap-y-2 md:gap-y-4 lg:basis-5/6 mx-6 w-full">
                     @csrf
                     <input type="hidden" name='formid' value="{{ $forms->id }}">
-                    <div class="flex md:flex-row flex-col items-start gap-x-4 w-full">
+                    <div class="flex md:flex-row flex-col items-start gap-x-4 w-full form-containers">
                         <div class="md:basis-1/2 w-full">
                             <input type="text" id="full-name" name="الاسم الكامل" placeholder="الاٍسم الكامل"
-                                class="mt-1 block w-full border-[#207ec6] py-3 font-black placeholder-black text-black rounded-md shadow-sm focus:border-none focus:ring-2 focus:ring-opacity-50 "
+                                class="mt-1 block w-full border-[#207ec6] py-3 font-black placeholder-black text-black rounded-md shadow-sm focus:border-none focus:ring-2 focus:ring-opacity-50"
                                 required>
                         </div>
                         <div class="md:basis-1/2 w-full mt-2 md:mt-0">
                             <input type="tel" id="phone" name="الهاتف" placeholder="الهاتف"
-                                class="mt-1 block w-full border-[#207ec6] py-3 font-black text-right placeholder-black text-black rounded-md shadow-sm focus:border-none focus:ring-2 focus:ring-opacity-50 "
+                                class="mt-1 block w-full border-[#207ec6] py-3 font-black text-right placeholder-black text-black rounded-md shadow-sm focus:border-none focus:ring-2 focus:ring-opacity-50"
                                 required>
                         </div>
                     </div>
-                    <div class="w-full">
-                        <textarea id="note" name="note" placeholder="ملاحظات" rows="10"
-                            class="mt-1 block w-full border-[#207ec6] placeholder-black text-black rounded-md shadow-sm focus:border-none focus:ring-2 focus:ring-opacity-50 "></textarea>
+                    <div class="w-full form-containers">
+                        <textarea id="note" name="note" placeholder="ملاحظات" rows="10" required
+                            class="mt-1 block w-full border-[#207ec6] placeholder-black text-black rounded-md shadow-sm focus:border-none focus:ring-2 focus:ring-opacity-50"></textarea>
                     </div>
-
-                    <div class="flex md:flex-row flex-col gap-y-2 my-8 md:my-0  items-center justify-between w-full">
+                    <div
+                        class="flex md:flex-row flex-col gap-y-2 my-8 md:my-0 items-center justify-between w-full form-containers">
                         <p class="text-[#207ec6] text-lg">
                             لمزيد من المعلومات يمكن التواصل :0507274834
                         </p>
@@ -92,7 +94,15 @@
                 </form>
                 <img src="{{ asset('assets/images/man.png') }}" alt="blue"
                     class="absolute left-[13%] top-[14%] max-h-[700px] hidden xl:block">
-
+            </div>
+            <div id="thank-you-message" class="hidden text-center w-full  mt-8 mb-12 xl:mt-36 xl:mb-20">
+                <div class="flex flex-col items-center justify-center gap-y-3">
+                    <h1 class="text-2xl font-bold text-gray-800">تم إرسال طلبك بنجاح!</h1>
+                    <p class="mt-4 text-lg text-gray-600">شكراً لك على تقديم طلبك. سنتواصل معك قريباً.</p>
+                    <p class="text-[#207ec6] text-lg mt-4">
+                        لمزيد من المعلومات يمكن التواصل :0507274834
+                    </p>
+                </div>
             </div>
         </div>
         <img src="{{ asset('assets/images/green.png') }}" alt="blue" class="absolute bottom-0 xl:block hidden">
@@ -105,6 +115,8 @@
     <script>
         document.addEventListener('DOMContentLoaded', function() {
             const form = document.getElementById('submission-form');
+            const formContainers = document.querySelectorAll('.form-containers');
+            const thankYouMessage = document.getElementById('thank-you-message');
             if (form) {
                 form.addEventListener('submit', function(event) {
                     event.preventDefault();
@@ -114,10 +126,14 @@
                         data[key] = value;
                     });
 
-                    axios.get('/sendform', data)
+                    axios.post('/formstore', data)
                         .then(response => {
-                            console.log('Form submitted successfully:', response);
-                            // Optionally hide the form and show a thank you message
+                            console.log("🚀 ~ form.addEventListener ~ response:", response)
+                            if (response.status == 200) {
+                                console.log("xxx")
+                            }
+                            formContainers.forEach(container => container.style.display = 'none');
+                            thankYouMessage.style.display = 'block';
                         })
                         .catch(err => console.log('Error submitting form:', err));
                 });
