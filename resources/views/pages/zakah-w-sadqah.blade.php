cdd3dd01ee1da9c9244f54a42a39576ed0bf2fb7
ameedasmah1 <ameed.asmah1@gmail.com>
2024-08-11 12:09:15 +0300

    Add vite

--- resources/views/pages/zakah-w-sadqah.blade.php
+++ resources/views/pages/zakah-w-sadqah.blade.php
@@ -1,16 +1,19 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta property="og:title" content="Support Zakah and Sadqah">
     <meta property="og:image" content="{{ asset('assets/images/zakah-w-sadaqah_13.jpeg') }}" />
-    <meta property="og:description" content="للتبرع بحواله بنكيه بنك مركنتيل فرع 699 حساب رقم 101805324 לגנת אלزכאת אלקוטריה . وهناك امكانية سحب بطاقة اعتماد ،bit من خلال الدخول للرابط :">
+    <meta property="og:description"
+        content="للتبرع بحواله بنكيه بنك مركنتيل فرع 699 حساب رقم 101805324 לגנת אלزכאת אלקוטריה . وهناك امكانية سحب بطاقة اعتماد ،bit من خلال الدخول للرابط :">
     <meta property="og:type" content="website">
     <meta property="og:url" content="{{ url('/zakah-w-sadqah') }}">
-    <meta name="twitter:card" content="summary_large_image"
-    <title>حملة تبرع كسوة طالب</title>
+    <meta name="twitter:card" content="summary_large_image" @vite('resources/css/app.css') @vite(['resources/sass/app.scss', 'resources/js/app.js']) <title>حملة تبرع كسوة
+    طالب</title>
 </head>
+
 <body>
     <div class="mx-auto max-w-7xl sm:px-6 lg:px-8 mt-32">
         <div class="flex flex-col items-center justify-center gap-y-4 text-lg">
@@ -22,11 +25,12 @@
             <p>سيتم إعادة توجيهك بعد لحظات...</p>
         </div>
     </div>
-{{--
+    {{--
     <script>
         setTimeout(function() {
             window.location.href = "https://secure.cardcom.solutions/EA/EA5/8NhphoWNYEyiRKr2vaaTiA/Intro";
         }, 3000);
     </script> --}}
 </body>
+
 </html>
